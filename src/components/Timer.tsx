4336e139f4bc8cf05fa3b43d099ef49d021b2f8e
hareeshkarni <karnihareesh@gmail.com>
2025-04-14 15:45:25 +0530

    🐛 fix: remove unused imports for Vercel build

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Alert } from "@/components/ui/alert";
 
 const Timer = ({
   duration = 30,
--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import SentenceQuestion from "@/components/SentenceQuestion";
 import Timer from "@/components/Timer";
